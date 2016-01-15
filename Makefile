6dbbda5e4406dcefc9fc19cda03312b88691f990
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2016-01-15 10:07:04 +0100

    Improving benchmarking

--- Makefile
+++ Makefile
@@ -2,12 +2,18 @@ CFLAGS=-I/smasher/src -O3 -Wall -Werror
 BUILD_DIR=build
 
 .PHONY: run_benchmark
-run_benchmark: $(BUILD_DIR)/benchmark
-	./$<
+run_benchmark: $(BUILD_DIR)/gdc_benchmark $(BUILD_DIR)/ldc_benchmark $(BUILD_DIR)/dmd_benchmark
+	$(BUILD_DIR)/gdc_benchmark
+	$(BUILD_DIR)/ldc_benchmark
+	$(BUILD_DIR)/dmd_benchmark
 
-$(BUILD_DIR)/benchmark: benchmark.d murmurhash3.d $(BUILD_DIR)/CMurmurHash3.o
-#	~/Downloads/gdc_x86_64-pc-linux-gnu/bin/gdc -O3 -frelease -Wall -Werror $^ -o$@
-#	~/Downloads/ldc2-0.16.1-linux-x86_64/bin/ldc2 -O -release -inline $^ -of$@
+$(BUILD_DIR)/gdc_benchmark: benchmark.d murmurhash3.d $(BUILD_DIR)/CMurmurHash3.o
+	~/Downloads/gdc_x86_64-pc-linux-gnu/bin/gdc -O3 -frelease -fno-bounds-check -Wall -Werror $^ -o$@
+
+$(BUILD_DIR)/ldc_benchmark: benchmark.d murmurhash3.d $(BUILD_DIR)/CMurmurHash3.o
+	~/Downloads/ldc2-0.16.1-linux-x86_64/bin/ldc2 -O5 -release -inline $^ -of$@
+
+$(BUILD_DIR)/dmd_benchmark: benchmark.d murmurhash3.d $(BUILD_DIR)/CMurmurHash3.o
 	dmd -O -inline -release $^ -of$@
 
 $(BUILD_DIR)/CMurmurHash3.o: smasher/src/MurmurHash3.cpp | $(BUILD_DIR)
--- benchmark.d
+++ benchmark.d
@@ -1,44 +1,61 @@
-extern(C++) {
+extern(C++)
+{
 	void MurmurHash3_x86_32  ( const(void*) key, int len, uint seed, void * out_ );
 	void MurmurHash3_x86_128 ( const(void*) key, int len, uint seed, void * out_ );
 	void MurmurHash3_x64_128 ( const(void*) key, int len, uint seed, void * out_ );
 }
 
-void main() {
+void consume(void[] buf)
+{
+version(GNU){
+	ubyte[16] tmp;
+	.MurmurHash3_x86_32(buf.ptr, 0, 0, &tmp);
+} else {
+	import core.bitop : volatileLoad;
+	volatileLoad(cast(ubyte*)buf.ptr);
+}
+}
+
+void main()
+{
 	import std.stdio;
 	immutable _Ki = 1024UL;
 	immutable _SIZE = 256UL * _Ki; // 256 KiB
 	ubyte[] buffer = new ubyte[_SIZE];
 	buffer[] = 0xAC;
-	auto HashC(alias H)() {
-		ubyte[16] output;
-		H(buffer.ptr, cast(int)(buffer.length), 0U, &output);
-		return output;
+	void HashC(alias H)() {
+		ubyte[16] tmp;
+		H(buffer.ptr, cast(int)(buffer.length), 0U, &tmp);
+		consume(tmp);
 	}
         import murmurhash3;
-	auto useHasher(H)() {
+	void useHasher(H)() {
 		H hasher;
 		hasher.putBlocks(cast(const(H.Block)[])buffer);
 		hasher.finalize();
-		return hasher.getBytes();
+		consume(hasher.getBytes());
         }
-        auto useDigestAPI(H)() {
-		return digest!(Piecewise!H)(buffer);
+        void useDigestAPI(H)() {
+		consume(digest!(Piecewise!H)(buffer));
         }
 	import std.datetime : benchmark;
         immutable times = (_Ki^^3) / _SIZE;
         writeln("Please wait while benchmarking MurmurHash3, running ", times, "*hash(256KiB) = 1GiB");
-        immutable descriptions = [
+        auto descriptions = [
 		"C++ MurmurHash3_x64_128", "D SMurmurHash3_x64_128", "D digest MurmurHash3_x64_128",
 		"C++ MurmurHash3_x86_128", "D SMurmurHash3_x86_128", "D digest MurmurHash3_x86_128",
 		"C++ MurmurHash3_x86_32",  "D SMurmurHash3_x86_32",  "D digest MurmurHash3_x86_32",
 	];
-        foreach (i, result; benchmark!(
+	auto results = benchmark!(
 		HashC!(.MurmurHash3_x64_128),useHasher!SMurmurHash3_x64_128,useDigestAPI!SMurmurHash3_x64_128,
 		HashC!(.MurmurHash3_x86_128),useHasher!SMurmurHash3_x86_128,useDigestAPI!SMurmurHash3_x86_128,
 		HashC!(.MurmurHash3_x86_32),useHasher!SMurmurHash3_x86_32,useDigestAPI!SMurmurHash3_x86_32,
-		)(times))
+		)(times);
+	import std.algorithm;
+	const fastest = results.reduce!min;
+	import std.range : lockstep;
+        foreach ( i, result; results)
         {
-            writefln("%-30s - %.0f GiB/s", descriptions[i], times * 1000. / result.msecs);
+            writefln("%-30s - %3d%% - %.0f GiB/s", descriptions[i], 100 * fastest.msecs / result.msecs, times * 1000. / result.msecs);
         }
 }
